2114297e302b7c3258b95257acbd20d942ba18ee
March.Anastasiia <nastya.marchenko00@gmail.com>
2025-12-16 12:05:11 +0300

    сделан левый сайдбар на главное странице, немного настроены шрифты

---
+++ index.js
@@ -0,0 +1,41 @@
+
+
+const burgerButton = document.querySelector('.burger_button');
+const sidebar = document.querySelector('.sidebar');
+const header = document.querySelector('.header_head');
+
+function updateSidebarPosition() {
+    const headerHeight = header.offsetHeight;
+    sidebar.style.top = headerHeight + 'px';
+    sidebar.style.height = `calc(100vh - ${headerHeight}px)`;
+}
+
+function openMenu() {
+    burgerButton.classList.add('active');
+    sidebar.classList.add('active');
+    updateSidebarPosition();
+}
+
+function closeMenu() {
+    burgerButton.classList.remove('active');
+    sidebar.classList.remove('active');
+}
+
+burgerButton.addEventListener('click', () => {
+    sidebar.classList.contains('active') ? closeMenu() : openMenu();
+})
+
+document.addEventListener('click', (e) => {
+    if (sidebar.classList.contains('active')
+        && !sidebar.contains(e.target)
+        && !burgerButton.contains(e.target)
+    ) {
+        closeMenu();
+    }
+})
+
+window.addEventListener("resize", () => {
+    if (sidebar.style.display === 'block') {
+        updateSidebarPosition();
+    }
+});
