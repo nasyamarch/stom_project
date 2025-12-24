f4c279468c3f0569fea179a008e154c1a1a495d8
March.Anastasiia <nastya.marchenko00@gmail.com>
2025-12-24 19:16:14 +0300

    поправлены пути ждя страниц и стиля, в скрипт добавлена команда для перехода по абсолютному пути от корня

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,30 @@
 import { defineConfig } from 'vite';
 import nunjucks from 'vite-plugin-nunjucks';
+import fg from 'fast-glob';
+import { resolve } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
+const _filename = fileURLToPath(import.meta.url);
+const _dirname = dirname(_filename);
+
+const pages = fg.sync(["index.html", "articles/**/*.html" ])
+    .reduce((acc, file) => {
+        acc[file] = resolve(_dirname, file)
+        return acc
+    }, {});
+
 
 export default defineConfig({
+    base: "/",
     plugins: [
         nunjucks({
-            templatesDir: 'src/templates', // путь к твоим шаблонам и страницам
-            outputDir: 'dist'     // куда будет собираться готовый HTML
+            templatesDir: 'src/templates' // путь к твоим шаблонам и страницам
         })
-    ]
+    ],
+    build: {
+        rollupOptions: {
+            input: pages
+        }
+    }
 });
